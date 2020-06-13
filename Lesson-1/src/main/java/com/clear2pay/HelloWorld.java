18354dc8372d270f0cb3d8f71fe533d39c30ad1b
Pankaj Jaiswal <jaiswal.pankaj10@gmail.com>
2020-06-13 17:27:52 +0530

    Add files via upload

---
+++ Lesson-1/src/main/java/com/clear2pay/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.clear2pay;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ Lesson-4/docker-machine.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Swarm mode using Docker Machine
+
+managers=2
+workers=2
+
+# create manager machines
+echo "======> Creating $managers manager machines ...";
+for node in $(seq 1 $managers);
+do
+	echo "======> Creating manager$node machine ...";
+	docker-machine create -d virtualbox manager$node;
+done
+
+# create worker machines
+echo "======> Creating $workers worker machines ...";
+for node in $(seq 1 $workers);
+do
+	echo "======> Creating worker$node machine ...";
+	docker-machine create -d virtualbox worker$node;
+done
+
+# list all machines
+docker-machine ls
+
+
